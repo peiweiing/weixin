0cd8f6ec194a2e4dd85a7ff74d215dd28e25be11
jingtianlong <engineerrobin@163.com>
2019-08-12 12:10:02 +0800

    提交

---
+++ component/production/production.js
@@ -0,0 +1,30 @@
+// component/production/production.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    list:[
+      { title: "水密码滢亮皙白滋润面膜", price:"12",src:"../../img/tu2.png"},
+      { title: "海藻水漾盈润细肤水120ml ）", price: "54", src: "../../img/tu01.jpg" },
+      { title: "皙白滋润柔肤水120ml", price: "78", src: "../../img/tu03.jpg" },
+      { title: "水密码滢亮皙白滋润柔肤水120ml", price: "34", src: "../../img/tu02.png" },
+      { title: "白滋润柔肤水120ml", price: "45", src: "../../img/tu1.jpg" },
+      { title: "柔肤水120ml", price: "98", src: "../../img/tu04.jpg" },
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
