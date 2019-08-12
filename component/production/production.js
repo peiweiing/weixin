03d0804de8f7be0b43b3782a661f59e85b239ac2
jingtianlong <engineerrobin@163.com>
2019-08-12 12:24:43 +0800

    ok

--- component/production/production.js
+++ component/production/production.js
@@ -15,7 +15,7 @@ Component({
       { title: "水密码滢亮皙白滋润面膜", price:"12",src:"../../img/tu2.png"},
       { title: "海藻水漾盈润细肤水120ml ）", price: "54", src: "../../img/tu01.jpg" },
       { title: "皙白滋润柔肤水120ml", price: "78", src: "../../img/tu03.jpg" },
-      { title: "水密码滢亮皙白滋润柔肤水120ml", price: "34", src: "../../img/tu02.png" },
+      { title: "水密码滢亮皙白滋润柔肤水120ml", price: "34", src: "../../img/tu02.jpg" },
       { title: "白滋润柔肤水120ml", price: "45", src: "../../img/tu1.jpg" },
       { title: "柔肤水120ml", price: "98", src: "../../img/tu04.jpg" },
     ]
